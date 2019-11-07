fef13f903a4b576dfb10ad9f275f517ddca1991f
martincosler <780953581@qq.com>
2019-11-07 22:32:27 +0800

    add a label in metric redis_instance_info named "redis_master_addr" and the value is "<masterIp>:<masterPort>" that the sentinels monitoring

--- exporter.go
+++ exporter.go
@@ -321,7 +321,7 @@ func NewRedisExporter(redisURI string, opts ExporterOptions) (*Exporter, error) 
 		"db_keys":                              {txt: "Total number of keys by DB", lbls: []string{"db"}},
 		"db_keys_expiring":                     {txt: "Total number of expiring keys by DB", lbls: []string{"db"}},
 		"exporter_last_scrape_error":           {txt: "The last scrape error status.", lbls: []string{"err"}},
-		"instance_info":                        {txt: "Information about the Redis instance", lbls: []string{"role", "redis_version", "redis_build_id", "redis_mode", "os"}},
+		"instance_info":                        {txt: "Information about the Redis instance", lbls: []string{"role", "redis_version", "redis_build_id", "redis_mode", "os", "redis_master_addr"}},
 		"key_size":                             {txt: `The length or size of "key"`, lbls: []string{"db", "key"}},
 		"key_value":                            {txt: `The value of "key"`, lbls: []string{"db", "key"}},
 		"last_slow_execution_duration_seconds": {txt: `The amount of time needed for last slow execution, in seconds`},
@@ -770,12 +770,15 @@ func (e *Exporter) extractInfoMetrics(ch chan<- prometheus.Metric, info string, 
 		}
 	}
 
+	instanceInfo["redis_master_addr"] = e.redisAddr
+
 	e.registerConstMetricGauge(ch, "instance_info", 1,
 		instanceInfo["role"],
 		instanceInfo["redis_version"],
 		instanceInfo["redis_build_id"],
 		instanceInfo["redis_mode"],
-		instanceInfo["os"])
+		instanceInfo["os"],
+		instanceInfo["redis_master_addr"])
 
 	if instanceInfo["role"] == "slave" {
 		e.registerConstMetricGauge(ch, "slave_info", 1,
